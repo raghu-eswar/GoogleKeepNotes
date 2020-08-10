1cc48b5a75c242cb0d960192f5b71edd69613754
raghueswar55 <raghueswar55@gmail.com>
2020-08-10 20:48:38 +0530

    Implemented forgot password screen and styled.

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { styles } from '../styles/forgotPassword.styles';
+import Button from '../components/Button';
 
 export default function ForgotPasswordScreen() {
 
+const [email, setEmail] = useState('');
+
   return (
-    <SafeAreaView></SafeAreaView>
+    <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView style={styles.content} behavior='position'>
+         <Text style={styles.message}>Please enter your registered email </Text>
+          <TextInput  style={styles.input} 
+                      keyboardType='email-address'
+                      placeholder='Email'
+                      value={email}
+                      onChangeText={(email) => setEmail(email)}/>
+          <View style={styles.buttons}>
+            <Button title='Confirm' style={styles.confirmButton}/>
+          </View>
+        </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
---
+++ src/styles/forgotPassword.styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+  },
+  content: {
+    width: '100%', 
+    height: '60%',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  message: {
+    alignSelf: 'center',
+    margin: 20,
+    fontWeight: "700",
+    fontSize: 20,
+    letterSpacing: 0.8,
+    color: '#fcb70b'
+  },
+  input: {
+    margin: 10,
+    width: 300,
+    height: 50,
+    borderColor: '#fdd36c',
+    borderWidth: 2,
+    borderRadius: 50,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },  
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    margin: 10,
+  },
+  confirmButton: {
+    width: 150,
+    height: 40,
+    fontSize: 18,
+    borderColor:'#ffffff', 
+    backgroundColor:'#fccc54', 
+    color:'#ffffff',
+  },
+});
