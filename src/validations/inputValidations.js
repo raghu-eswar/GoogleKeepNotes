0128a9a05a458b90f1de3e39e5461c98867d25cb
raghueswar55 <raghueswar55@gmail.com>
2020-08-14 11:09:05 +0530

    [ Raghu ] REFACTORED: modified inputValidations to return boolean.

--- src/validations/inputValidations.js
+++ src/validations/inputValidations.js
@@ -1,17 +1,11 @@
-export function emailValidator(email, setError) {
-  if (!/^[a-zA-Z0-9$&+_-]+(\.[a-zA-Z0-9]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-z]{2,4}$/.test(email))
-    setError("invalied user name");
-  else setError("");
+export function emailValidator(email) {
+  return /^[a-zA-Z0-9$&+_-]+(\.[a-zA-Z0-9]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-z]{2,4}$/.test(email)
 }
 
-export function passwordValidator(password, setError) {
-  if (!/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}/.test(password))
-    setError("invalied Password");
-  else setError("");
+export function passwordValidator(password) {
+  return /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}/.test(password)
 }
 
-export function nameValidator(name, setError) {
-  if (!/^[A-Z][a-z]{3,}$/.test(name))
-    setError("invalied Name");
-  else setError("");
+export function nameValidator(name) {
+  return /^[A-Z][a-z]{3,}$/.test(name)
 }
