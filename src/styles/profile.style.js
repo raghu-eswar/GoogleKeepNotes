8a2d5833a0efd0705a38a6d2ebc4e6ab8d7ed47b
raghueswar55 <raghueswar55@gmail.com>
2020-08-17 10:38:38 +0530

    [ Raghu ] ADD: added styles for profile screen.

---
+++ src/styles/profile.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  navigatorItemStyle: {
+    borderBottomEndRadius: 50,
+    borderTopEndRadius: 50,
+  },
+  navigatorStyle: {
+    paddingTop: 30,
+  },
+});
