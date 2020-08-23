a0d55be960c9393212bc7a48ba4dbf3998e824e7
raghueswar55 <raghueswar55@gmail.com>
2020-08-23 15:57:31 +0530

    [ Raghu ] REAFACTORED: modified plus button styles.

--- src/components/PlusButton.js
+++ src/components/PlusButton.js
@@ -1,31 +1,45 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { Button } from "react-native-elements";
 
 export default function PlusButton(props) {
-
   return (
-    <Button
-      type="outline"
-      buttonStyle={style.buttonStyle}
-      containerStyle={style.containerStyle}
-      icon={<Icon name="plus" size={50} color="black" />}
-      {...props}
-    />
+    <View style={style.containerStyle}>
+      <Button
+        buttonStyle={style.buttonStyle}
+        containerStyle={style.contentStyle}
+        icon={<Icon name="plus" size={50} color="black" />}
+        {...props}
+      />
+    </View>
   );
 }
 
 const style = StyleSheet.create({
-    buttonStyle:{
-        backgroundColor: "transparent",
-        padding: 0,
-      },
-      containerStyle:{
-        position: "absolute",
-        right: 30,
-        bottom: 120,
-        width: 50,
-        borderRadius: 50,
-      },
-  });
+  buttonStyle: {
+    backgroundColor: "transparent",
+    padding: 0,
+  },
+  contentStyle: {
+    backgroundColor: "white",
+    width: 50,
+    borderRadius: 25,
+    elevation: 10,
+    transform: [{ rotate: "45deg" }],
+  },
+  containerStyle: {
+    padding: 5,
+    backgroundColor: "#fff",
+    borderWidth: 1.5,
+    borderRadius: 40,
+    borderTopColor: "lightgray",
+    borderBottomColor: "transparent",
+    borderRightColor: "transparent",
+    borderLeftColor: "lightgray",
+    left: "80%",
+    bottom: "59%",
+    width: "16%",
+    transform: [{ rotate: "-134deg" }],
+  },
+});
